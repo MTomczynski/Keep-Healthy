03d6961f84d95ebc1235485f47a60514c0bd83d1
maciek <mj.tomczynski@gmail.com>
2019-07-26 21:12:25 +0200

    Added test implementation for registering notification on save event

--- lib/data/ui/work_hours_bloc.dart
+++ lib/data/ui/work_hours_bloc.dart
@@ -1,10 +1,14 @@
 import 'package:bloc/bloc.dart';
+import 'package:keep_healthy/data/model/notification_rule.dart';
 import 'package:keep_healthy/data/repository/work_hours_repository.dart';
 import 'package:keep_healthy/data/ui/work_hours_event.dart';
 import 'package:keep_healthy/data/ui/work_hours_state.dart';
 
+import '../notifications_manager.dart';
+
 class WorkHoursBloc extends Bloc<WorkHoursEvent, WorkHoursState> {
   final WorkHoursRepository workHoursRepository;
+  final NotificationManager notificationManager = NotificationManager();
 
   WorkHoursBloc(this.workHoursRepository);
 
@@ -23,6 +27,10 @@ class WorkHoursBloc extends Bloc<WorkHoursEvent, WorkHoursState> {
       return;
     } else if (event is SaveWorkHours) {
       await workHoursRepository.saveWorkHours(event.workHours);
+      notificationManager.clearNotifications();
+      NotificationRule rule =
+          NotificationRule(1, "Test rule", "Testing", "Just testing", 1);
+      notificationManager.setupDailyNotifications(event.workHours, rule);
       yield WorkHoursLoaded(event.workHours);
       return;
     } else if (event is ShowTempWorkHours) {
