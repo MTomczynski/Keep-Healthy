d0e27293cf6138dedc3be73f6142827401ff6c34
maciek <mj.tomczynski@gmail.com>
2019-07-26 23:31:15 +0200

    Fixed missing id usage from NotificationRule

--- lib/data/notifications_manager.dart
+++ lib/data/notifications_manager.dart
@@ -3,11 +3,9 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'model/notification_rule.dart';
 import 'model/work_hours.dart';
 
-
 initializeLocalNotificationsPlugin() {
   final plugin = FlutterLocalNotificationsPlugin();
-  var initializationSettingsAndroid =
-  AndroidInitializationSettings('app_icon');
+  var initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
   var initializationSettingsIOS = IOSInitializationSettings();
   var initializationSettings = InitializationSettings(
       initializationSettingsAndroid, initializationSettingsIOS);
@@ -16,7 +14,8 @@ initializeLocalNotificationsPlugin() {
 }
 
 class NotificationManager {
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = initializeLocalNotificationsPlugin();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      initializeLocalNotificationsPlugin();
 
   void clearNotifications() {
     flutterLocalNotificationsPlugin.cancelAll();
@@ -26,24 +25,32 @@ class NotificationManager {
       WorkHours workHours, NotificationRule notificationRule) async {
     List<Time> notificationTimes = List<Time>();
 
-    var notificationTime = workHours.startTime.add(Duration(minutes: notificationRule.intervalMinutes));
+    var notificationTime = workHours.startTime
+        .add(Duration(minutes: notificationRule.intervalMinutes));
 
     do {
-      var tmp = Time(notificationTime.hour, notificationTime.minute, notificationTime.second);
+      var tmp = Time(notificationTime.hour, notificationTime.minute,
+          notificationTime.second);
 
       notificationTimes.add(tmp);
-      notificationTime = notificationTime.add(Duration(minutes: notificationRule.intervalMinutes));
-
+      notificationTime = notificationTime
+          .add(Duration(minutes: notificationRule.intervalMinutes));
     } while (notificationTime.isBefore(workHours.endTime));
 
-    final platformSpecificRules = createPlatformChannelSpecifics(notificationRule);
-    for(int i = 0; i < notificationTimes.length; i++) {
+    final platformSpecificRules =
+        createPlatformChannelSpecifics(notificationRule);
+    for (int i = 0; i < notificationTimes.length; i++) {
       final notificationTime = notificationTimes[i];
-      setupDailyNotification(i, platformSpecificRules, notificationTime, notificationRule);
+      setupDailyNotification(
+          i, platformSpecificRules, notificationTime, notificationRule);
     }
   }
 
-  void setupDailyNotification(int id, NotificationDetails platformChannelSpecifics, Time time, NotificationRule notificationRule) async{
+  void setupDailyNotification(
+      int id,
+      NotificationDetails platformChannelSpecifics,
+      Time time,
+      NotificationRule notificationRule) async {
     await flutterLocalNotificationsPlugin.showDailyAtTime(
         id,
         notificationRule.notificationTitle,
@@ -52,16 +59,15 @@ class NotificationManager {
         platformChannelSpecifics);
   }
 
-  NotificationDetails createPlatformChannelSpecifics(NotificationRule notificationRule){
-    var androidPlatformChannelSpecifics =
-    AndroidNotificationDetails(notificationRule.id.toString(),
-        notificationRule.ruleName, notificationRule.ruleName, icon: "app_icon", largeIcon: "app_icon");
-    var iOSPlatformChannelSpecifics =
-    IOSNotificationDetails();
+  NotificationDetails createPlatformChannelSpecifics(
+      NotificationRule notificationRule) {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        "0", notificationRule.ruleName, notificationRule.ruleName,
+        icon: "app_icon", largeIcon: "app_icon");
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
     var platformChannelSpecifics = NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
 
     return platformChannelSpecifics;
   }
-
 }
