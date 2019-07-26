6d82dc0626361b5d9a66a59efe7b3a828ab8fe9c
maciek <mj.tomczynski@gmail.com>
2019-07-26 19:09:57 +0200

    Added Equatable implementation in state

--- lib/data/ui/work_hours_state.dart
+++ lib/data/ui/work_hours_state.dart
@@ -1,8 +1,13 @@
 
+import 'dart:core';
+
 import 'package:equatable/equatable.dart';
 import 'package:keep_healthy/data/model/work_hours.dart';
 
-abstract class WorkHoursState extends Equatable {}
+abstract class WorkHoursState extends Equatable {
+
+  WorkHoursState([List props = const []]) : super(props);
+}
 
 class WorkHoursUninitialized extends WorkHoursState {
 
@@ -16,12 +21,10 @@ class WorkHoursLoaded extends WorkHoursState {
 
   WorkHours workHours;
 
-  WorkHoursLoaded(this.workHours);
+  WorkHoursLoaded(this.workHours) : super([workHours]);
 
   @override
   String toString() {
     return 'WorkHoursLoaded ${workHours.startTime}, ${workHours.endTime}';
   }
 }
-
-
