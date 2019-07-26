357ef7e4a6db06209a9014b963954c9e4e66f4ed
maciek <mj.tomczynski@gmail.com>
2019-07-26 21:11:25 +0200

    Added unique ids for registering notifications

--- lib/data/notifications_manager.dart
+++ lib/data/notifications_manager.dart
@@ -7,27 +7,34 @@ class NotificationManager {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       new FlutterLocalNotificationsPlugin();
 
+  void clearNotifications() {
+    flutterLocalNotificationsPlugin.cancelAll();
+  }
+
   void setupDailyNotifications(
       WorkHours workHours, NotificationRule notificationRule) async {
     List<Time> notificationTimes = new List<Time>();
-    
-    var notificationTime = workHours.startTime.add(Duration(minutes: notificationRule.intervalMinutes));
+
+    var notificationTime = workHours.startTime.add(new Duration(minutes: notificationRule.intervalMinutes));
 
     do {
       var tmp = Time(notificationTime.hour, notificationTime.minute, notificationTime.second);
-      
+
       notificationTimes.add(tmp);
-      notificationTime.add(Duration(minutes: notificationRule.intervalMinutes));
+      notificationTime = notificationTime.add(new Duration(minutes: notificationRule.intervalMinutes));
 
     } while (notificationTime.isBefore(workHours.endTime));
-    
-    notificationTimes.forEach((notificationTime) =>
-        setupDailyNotification(createPlatformChannelSpecifics(notificationRule), notificationTime, notificationRule));
+
+    final platformSpecificRules = createPlatformChannelSpecifics(notificationRule);
+    for(int i = 0; i < notificationTimes.length; i++) {
+      final notificationTime = notificationTimes[i];
+      setupDailyNotification(i, platformSpecificRules, notificationTime, notificationRule);
+    }
   }
 
-  void setupDailyNotification(NotificationDetails platformChannelSpecifics, Time time, NotificationRule notificationRule) async{
+  void setupDailyNotification(int id, NotificationDetails platformChannelSpecifics, Time time, NotificationRule notificationRule) async{
     await flutterLocalNotificationsPlugin.showDailyAtTime(
-        notificationRule.id,
+        id,
         notificationRule.notificationTitle,
         notificationRule.notificationMessage,
         time,
@@ -37,13 +44,13 @@ class NotificationManager {
   NotificationDetails createPlatformChannelSpecifics(NotificationRule notificationRule){
     var androidPlatformChannelSpecifics =
     new AndroidNotificationDetails(notificationRule.id.toString(),
-        notificationRule.ruleName, notificationRule.ruleName);
+        notificationRule.ruleName, notificationRule.ruleName, icon: "app_icon", largeIcon: "app_icon");
     var iOSPlatformChannelSpecifics =
     new IOSNotificationDetails();
     var platformChannelSpecifics = new NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    
+
     return platformChannelSpecifics;
   }
-  
+
 }
