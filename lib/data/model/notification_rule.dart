7bc1fb5d1951605147418bdc12fc59639bb38dea
maciek <mj.tomczynski@gmail.com>
2019-07-26 23:21:05 +0200

    Removed unused id propertyfrom NotificationRule

--- lib/data/model/notification_rule.dart
+++ lib/data/model/notification_rule.dart
@@ -1,12 +1,11 @@
 import 'package:equatable/equatable.dart';
 
 class NotificationRule extends Equatable {
-  final int id;
   final String ruleName;
   final String notificationTitle;
   final String notificationMessage;
   final int intervalMinutes;
 
   NotificationRule(
-      this.id, this.ruleName, this.notificationTitle, this.notificationMessage, this.intervalMinutes);
+      this.ruleName, this.notificationTitle, this.notificationMessage, this.intervalMinutes);
 }
