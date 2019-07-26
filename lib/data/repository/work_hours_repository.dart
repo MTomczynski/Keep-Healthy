5d5b32862d59576a8359883abaa5d06e08afad48
maciejgalos <maciej@mobilereality.pl>
2019-07-26 15:37:35 +0200

    Added WorkHours repository.

---
+++ lib/data/repository/work_hours_repository.dart
@@ -0,0 +1,24 @@
+import 'package:keep_healthy/data/model/work_hours.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class WorkHoursRepository {
+  WorkHoursRepository();
+
+  Future<void> saveWorkHours(WorkHours workHours) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt(KEY_START_TIME, workHours.startTime.millisecondsSinceEpoch);
+    prefs.setInt(KEY_END_TIME, workHours.endTime.millisecondsSinceEpoch);
+  }
+
+  Future<WorkHours> getWorkHours() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    DateTime startDate = DateTime.fromMicrosecondsSinceEpoch(prefs.getInt(KEY_START_TIME));
+    DateTime endDate = DateTime.fromMicrosecondsSinceEpoch(prefs.getInt(KEY_END_TIME));
+
+    return WorkHours(startDate, endDate);
+  }
+
+  final String KEY_START_TIME = "start_time";
+  final String KEY_END_TIME = "end_time";
+}
